d03c9828137e68664c7b75bb51a7ce04d3ae8023
Arvind Suresh <arvind.suresh60@gmail.com>
2020-06-13 12:16:55 -0400

    Switch asio/asio to boost/asio

--- include/eelbot_framework/framework.hpp
+++ include/eelbot_framework/framework.hpp
@@ -1,6 +1,10 @@
+#ifndef EELBOT_FRAMEWORK_FRAMEWORK
+#define EELBOT_FRAMEWORK_FRAMEWORK
+
 #include <string>
 
 #include "eelbot_framework/bot.hpp"
 #include "eelbot_framework/conn.hpp"
 #include "eelbot_framework/msg.hpp"
 
+#endif
