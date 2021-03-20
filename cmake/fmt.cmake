65805432ef23ffab59db32279b4dd1ae4be5cc21
Arvind Suresh <arvind.suresh60@gmail.com>
2021-03-20 13:36:24 -0400

    Setup install target

--- cmake/fmt.cmake
+++ cmake/fmt.cmake
@@ -1,5 +1,5 @@
 if (NOT TARGET fmt)
-	add_subdirectory(${PROJECT_SOURCE_DIR}/3rdparty/fmt)
+	add_subdirectory(${PROJECT_SOURCE_DIR}/3rdparty/fmt EXCLUDE_FROM_ALL)
 endif()
 
 set(EF_LINK_LIBS ${EF_LINK_LIBS} fmt)
--- cmake/zlib.cmake
+++ cmake/zlib.cmake
@@ -1,5 +1,5 @@
 include_directories(${PROJECT_SOURCE_DIR}/3rdparty/zlib) # Workaround for https://github.com/madler/zlib/issues/218.
-add_subdirectory(${PROJECT_SOURCE_DIR}/3rdparty/zlib ${CMAKE_CURRENT_BINARY_DIR}/3rdparty/zlib)
+add_subdirectory(${PROJECT_SOURCE_DIR}/3rdparty/zlib ${CMAKE_CURRENT_BINARY_DIR}/3rdparty/zlib EXCLUDE_FROM_ALL)
 set(EF_ZLIB_INCLUDE_DIRS ${EF_ZLIB_INCLUDE_DIRS} ${PROJECT_SOURCE_DIR}/3rdparty/zlib ${CMAKE_CURRENT_BINARY_DIR}/3rdparty/zlib)
 set(EF_INCLUDE_DIRS ${EF_INCLUDE_DIRS} ${EF_ZLIB_INCLUDE_DIRS})
 set(EF_LINK_LIBS ${EF_LINK_LIBS} zlibstatic)
